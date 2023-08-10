3e9204e1947a9189decc09c73338e7e79ab9e08f
Hudson Souza (Own3d 私) <70814948+Hudsonsoouza@users.noreply.github.com>
2023-08-10 16:46:32 -0300

    select command

--- public/index.php
+++ public/index.php
@@ -3,10 +3,6 @@
 require_once('../src/config/config.php');
 require_once('../src/models/User.php');
 
-// $user = new User([]);
-
-// $user = new User(['name'=>'Lucas','email'=>'Lucas']);
-// print_r($user);
-// $user->email = 'lucas@gmail.com';
-// echo '<br>';
-// print_r($user);
+echo User::getSelect(['id' => '1'], 'name, email');
+echo '<br>';
+echo User::getSelect(['name' => 'Chaves', 'email' => 'chaves@cod3r.com.br']);
