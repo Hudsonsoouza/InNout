48afe9afaf51391953e9fb973a11575bb507ee25
Hudson Souza (Own3d 私) <70814948+Hudsonsoouza@users.noreply.github.com>
2023-08-11 15:52:51 -0300

    login page

--- public/index.php
+++ public/index.php
@@ -1,11 +1,4 @@
 <?php
 
 require_once('../src/config/config.php');
-require_once('../src/models/User.php');
-
- $user = new User(['name'=>'lucas', 'email'=>'lucas@gmail.com']);
-
-foreach(User::get([], 'name') as $user){
- echo $user->name;
- echo '<br>';
-}
+require_once('../src/views/login.php');
